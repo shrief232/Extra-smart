fa2da795e3b4bbe50161900e5bcaa2ed0bd04c7e
Shrief <shriefmansour@gmail.com>
2025-06-05 17:58:48 +0300

    api edit

--- extra_smart/src/api.js
+++ extra_smart/src/api.js
@@ -3,7 +3,7 @@ import { ACCESS_TOKEN, REFRESH_TOKEN } from './constants';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000',
+  baseURL: import.meta.env.VITE_API_URL ,
   withCredentials: true,
   xsrfCookieName: 'csrftoken',  
   xsrfHeaderName: 'X-CSRFToken',
